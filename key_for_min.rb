215927e30e278b2f9defb94b62101f536f3373e9
confident-token-1019 <theoonetj@gmail.com>
2019-12-20 02:52:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,3 +17,7 @@ end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 key_for_min_value(ikea)
+
+
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)
