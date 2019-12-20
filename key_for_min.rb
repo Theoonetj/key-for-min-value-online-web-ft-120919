1d117c09e70a01af3bbb6262fe7518ca6369e20b
confident-token-1019 <theoonetj@gmail.com>
2019-12-20 02:49:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,9 @@ smallest_key = 0
 smallest_value = nil 
 
 name_hash.each do |key, value|
-  
+  if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    smallest_key = key
+  end
+  smallest_key
 end
